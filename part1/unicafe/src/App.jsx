2e3cf2023a73936c1b020b5d7952ae3c1b7ed094
Caio Barros Dewnig <caio.dewnig@troxbrasil.com.br>
2025-12-10 14:03:51 -0300

    (Unicafe, step 2) - included

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -13,11 +13,9 @@ const App = () => {
       <button onClick={()=> setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={()=> setBad(bad + 1)}>Bad</button>
       <h2>Statistics</h2>
-      <p>Total number of "Good" responses: {good} ({Math.round((good*100)/(good + neutral + bad))}% of total responses)</p>
-      <p>Total number of "Neutral" responses: {neutral} ({Math.round((neutral*100)/(good + neutral + bad))}% of total responses)</p>
-      <p>Total number of "Bad" responses: {bad} ({Math.round((bad*100)/(good + neutral + bad))}% of total responses)</p>
-      <p>Total number of responses: {(good + neutral + bad)}</p>
-      <p>Average of responses: {((good + neutral + bad)/3).toFixed(2)}</p>
+      <p>Total number of "Good" responses: {good}</p>
+      <p>Total number of "Neutral" responses: {neutral}</p>
+      <p>Total number of "Bad" responses: {bad}</p>
     </>
   )
 }
