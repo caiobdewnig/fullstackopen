4f6128581350bd6745f87cbb8fa234fa439bccfc
Caio Barros Dewnig <caio.dewnig@troxbrasil.com.br>
2025-12-10 08:14:44 -0300

    (Course Information, step 4) - included

--- part1/course-information/src/App.jsx
+++ part1/course-information/src/App.jsx
@@ -22,26 +22,27 @@ function Content(p) {
 }
 
 const App = () => {
-  const courseChoice = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <>
-      <Header course={courseChoice} />
-      <Content part1={part1} part2={part2} part3={part3} />
+      <Header course={course} />
+      <Content part1={parts[0]} part2={parts[1]} part3={parts[2]} />
     </>
   )
 }
-
 export default App
