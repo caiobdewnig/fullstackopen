cf12ac25ed0e39843d7789ad1f6725b07046745a
Caio Barros Dewnig <caio.dewnig@troxbrasil.com.br>
2025-12-11 10:27:03 -0300

    (Unicafe, step 5 and 6) - included, and after searching W3Schools for pure HTML code snippets to create the tables, I took the liberty of creating a 'main.css' with simple CSS to include some borders, even though Fullstackopen didn't explicitly ask for them.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,36 +1,89 @@
 import { useState } from 'react'
+import './main.css'
 
-const Statistics = ({gooD, neutraL, baD}) => {
-  if (!gooD && !neutraL && !baD) {
-    return (<p>No feedback given.</p>)
-  } else {
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
+
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  
+  if (total === 0) {
     return (
-      <>
-        <p>Total number of "Good" responses: {gooD} - ({Math.round((gooD*100)/(gooD + neutraL + baD))}% of total responses)</p>
-        <p>Total number of "Neutral" responses: {neutraL} - ({Math.round((neutraL*100)/(gooD + neutraL + baD))}% of total responses)</p>
-        <p>Total number of "Bad" responses: {baD} - ({Math.round((baD*100)/(gooD + neutraL + baD))}% of total responses)</p>
-        <p>Total number of responses: {(gooD + neutraL + baD)}</p>
-        <p>Average of responses: {((gooD + neutraL + baD)/3).toFixed(2)}</p>
-      </>
+      <div>
+        <p>No feedback given</p>
+      </div>
     )
   }
+
+
+  const goodPct = (good * 100) / total
+  const neutralPct = (neutral * 100) / total
+  const badPct = (bad * 100) / total
+  const yourAverage = (total / 3) 
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine 
+          text="Total de respostas 'Good'" 
+          value={`${good} - (${goodPct.toFixed(0)}% do total)`} 
+          className="line"
+        />
+        <StatisticLine 
+          text="Total de respostas 'Neutral'" 
+          value={`${neutral} - (${neutralPct.toFixed(0)}% do total)`} 
+          className="line"
+        />
+        <StatisticLine 
+          text="Total de respostas 'Bad'" 
+          value={`${bad} - (${badPct.toFixed(0)}% do total)`} 
+          className="line"
+        />
+        <StatisticLine 
+          text="Total de respostas" 
+          value={total} 
+        />
+        <StatisticLine 
+          text="Média das respostas" 
+          value={yourAverage.toFixed(2)} 
+          className="line"
+        />
+      </tbody>
+    </table>
+  )
 }
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+
   return (
-    <>
+    <div>
       <h1>Give a Feedback!</h1>
-      <button onClick={()=> setGood(good + 1)}>Good</button>
-      <button onClick={()=> setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={()=> setBad(bad + 1)}>Bad</button>
+      <Button handleClick={handleGood} text="Good" />
+      <Button handleClick={handleNeutral} text="Neutral" />
+      <Button handleClick={handleBad} text="Bad" />
+      
       <h2>Statistics</h2>
-      <Statistics gooD={good} baD={bad} neutraL={neutral}/>
-    </>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
   )
 }
 
