7ec5819b9e7a55b26e4d6634ad517c9d4f40367f
Caio Barros Dewnig <caio.dewnig@troxbrasil.com.br>
2025-12-12 10:27:25 -0300

    Added a title, a footer and the reduce method to calculate the total of exercises in each course

--- part2/course-information2/src/App.jsx
+++ part2/course-information2/src/App.jsx
@@ -1,16 +1,17 @@
 const Course = ({course}) => {
   return (
     <>
-      <h1 key={course.id}>{course.name}</h1>
+      <h2 key={course.id}>{course.name}</h2>
       <ol key={course.id}>
         {course.parts.map((part, i) => {
           return (
             <>
-              <li key={part.id + i}>{part.name} ({part.exercises} exercises)</li>
+              <li key={part.id + i}>{part.name} - ({part.exercises} exercises)</li>
             </>
           )
         })}
-      </ol>  
+      </ol>
+      <b key={course.id}>Exercises in total for the "{course.name}" course: {course.parts.reduce((sum, order) => sum + order.exercises, 0)}</b>  
     </>
   )
 };
@@ -64,7 +65,22 @@ const App = () => {
 
   return (
     <div>
+      <h1>Web Development Course</h1>
       {courses.map((course) => <Course course={course}/>)}
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <footer>
+        <p>&copy; 2025 Quiago's Courses. All rights reserved.</p>
+        <p>Contact: <a href="mailto:quiagotimelei@gmail.com">quiagotimelei@gmail.com</a></p>
+      </footer>
     </div>
   )
 }
