e0e8be1a1cf1a4d6ee8d7d006aa76bbb141cb049
Caio Barros Dewnig <caio.dewnig@troxbrasil.com.br>
2026-01-05 09:06:26 -0300

    (Phonebook Backend, step 7 and 8) - added morgan to log custom content on the console

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,8 +4,15 @@ const morgan = require('morgan')
 
 
 app.use(express.json())
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
+morgan.token('req-body', (req) => {
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body);
+    }
+    return ''
+})
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body '))
 
 let list = [
     { 
